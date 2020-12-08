4cec56abb7fb6c14fbd38702ea299615a3ef62fd
furps <lukasznowacki@mail.com>
2020-12-08 13:33:03 +0100

    Adding dashboard view

--- src/main/java/com/loginregistration/controller/AppController.java
+++ src/main/java/com/loginregistration/controller/AppController.java
@@ -3,6 +3,7 @@ package com.loginregistration.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.loginregistration.model.Users;
 
@@ -13,5 +14,15 @@ public class AppController {
 	public String getLogin(Model model) {
 		model.addAttribute("user", new Users());
 		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin(Users user) {
+		return "redirect:/dashboard";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+		return "dashboard";
 	}
 }

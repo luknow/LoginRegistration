a10fa49f53c6d20bceb93872e4dd71c765721980
furps <lukasznowacki@mail.com>
2020-12-09 10:46:45 +0100

    Adding admin endpoint
    If user is logged as admin he will see admin's greetings

--- src/main/java/com/loginregistration/WebSecurityConfig.java
+++ src/main/java/com/loginregistration/WebSecurityConfig.java
@@ -43,6 +43,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/css/**").permitAll()
+		.antMatchers("/admin").hasRole("ADMIN")
 		.anyRequest().authenticated()
         .and()
         .formLogin()
--- src/main/java/com/loginregistration/controller/AppController.java
+++ src/main/java/com/loginregistration/controller/AppController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.loginregistration.model.Users;
 
@@ -24,5 +25,11 @@ public class AppController {
 	@GetMapping("/dashboard")
 	public String dashboard() {
 		return "dashboard";
+	}
+	
+	@GetMapping("/admin")
+	public String admin(RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute("success", "Witaj adminie!");
+		return "redirect:/dashboard";
 	}
 }
